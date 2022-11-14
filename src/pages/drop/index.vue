becfb6c3923175a78c6b411cfd73fe067bab8440
sikonggpw <1327325804@qq.com>
2022-11-14 22:50:20 +0800

    feat(drop):拖拽

---
+++ src/pages/drop/index.vue
@@ -0,0 +1,94 @@
+<!--
+ * @Author: sikonggpw 1327325804@qq.com
+ * @Date: 2022-11-14 22:22:55
+ * @LastEditors: sikonggpw 1327325804@qq.com
+ * @LastEditTime: 2022-11-14 22:48:28
+ * @FilePath: \snow-vue\src\pages\drop\index.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div class="list" ref="list">
+    <div draggable="true" class="list-item">1</div>
+    <div draggable="true" class="list-item">2</div>
+    <div draggable="true" class="list-item">3</div>
+    <div draggable="true" class="list-item">4</div>
+    <div draggable="true" class="list-item">5</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SnowVueIndex',
+
+  data() {
+    return {
+      suorceNode: null
+    };
+  },
+
+  mounted() {
+    this.init()
+  },
+
+  methods: {
+    init() {
+      const list = document.querySelector('.list')
+      list.ondragstart = e => {
+        setTimeout(() => {
+          e.target.classList.add('moving')
+        }, 0)
+        this.suorceNode = e.target
+        e.dataTransfer.effectAllowed = 'move'
+      }
+
+      list.ondragover = e => {
+        e.preventDefault()
+      }
+      
+      list.ondragenter = e => {
+        e.preventDefault();
+        if (e.target === list || e.target === this.suorceNode) {
+          return;
+        }
+        const children = Array.from(list.children)
+        const suorceIndex = children.indexOf(this.suorceNode)
+        const targetIndex = children.indexOf(e.target)
+        if (suorceIndex > targetIndex) {
+          // console.log('向上');
+          list.insertBefore(this.suorceNode, e.target)
+        } else {
+          // console.log('向下');
+          list.insertBefore(this.suorceNode, e.target.nextElementSibling)
+        }
+      }
+
+      list.ondragend = e => {
+        e.target.classList.remove('moving');
+      }
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.list {
+  margin: 100px 0 0 500px;
+
+  .list-item {
+    width: 300px;
+    height: 50px;
+    background-color: red;
+    margin-bottom: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+  }
+
+  .moving {
+    background: transparent;
+    color: transparent;
+    border: 1px dashed #ccc;
+  }
+}
+</style>
