bfac0a5433d05ed04649dd54ba1818e53060fbfa
sikonggpw <1327325804@qq.com>
2023-01-05 11:33:36 +0800

    feat(copy):复制功能实现 利用插件实现

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,58 @@
  * @Author: sikonggpw 1327325804@qq.com
  * @Date: 2022-12-07 10:49:38
  * @LastEditors: sikonggpw 1327325804@qq.com
- * @LastEditTime: 2022-12-26 23:35:12
+ * @LastEditTime: 2023-01-05 11:32:16
  * @FilePath: \snow-vue\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <div>
-    
+    <p>{{ message }}</p>
+
+    <button @click="copy" class="Collection-btn" :data-clipboard-text="message">复制</button>
   </div>
 </template>
 
 <script>
-  import { deepClone } from '../deepClone'
+import Clipboard from 'clipboard'
 export default {
-  name: 'SnowVueApp',
-
   data() {
     return {
+      message: 'Hello Vue!',
     };
   },
 
   mounted() {
-    const obj1 = {
-      a: 1,
-      b: 2,
-    }
-    const obj = deepClone(obj1)
-    console.log(obj === obj1, 'obj === this.obj')
+
   },
 
   methods: {
-    
+    copy() {
+      let clipboard = new Clipboard('.Collection-btn ')
+      console.log(clipboard)
+      clipboard.on('success', (e) => {
+        // 释放内存
+        console.log(e, '复制成功')
+        
+        clipboard.destroy()
+      })
+      clipboard.on('error', (e) => {
+        console.log(e, '复制失败');
+        // 释放内存
+        clipboard.destroy()
+      })
+    }
+    // copy() {
+    //   const input = document.createElement('input')
+    //   input.value = this.message
+    //   document.body.appendChild(input)
+    //   input.select()
+    //   if (document.execCommand('copy')) {
+    //     document.execCommand('copy')
+    //     console.log('复制成功')
+    //   }
+    //   document.body.removeChild(input)
+    // }
   },
 };
 </script>
---
+++ src/深拷贝.vue
@@ -0,0 +1,42 @@
+<!--
+ * @Author: sikonggpw 1327325804@qq.com
+ * @Date: 2022-12-07 10:49:38
+ * @LastEditors: sikonggpw 1327325804@qq.com
+ * @LastEditTime: 2022-12-26 23:35:12
+ * @FilePath: \snow-vue\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+  import { deepClone } from '../deepClone'
+export default {
+  name: 'SnowVueApp',
+
+  data() {
+    return {
+    };
+  },
+
+  mounted() {
+    const obj1 = {
+      a: 1,
+      b: 2,
+    }
+    const obj = deepClone(obj1)
+    console.log(obj === obj1, 'obj === this.obj')
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
