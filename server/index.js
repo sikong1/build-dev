b07b2eb294b139db7d119ee5a7c931e1efb36c4b
sikonggpw <1327325804@qq.com>
2023-06-02 11:49:42 +0800

    fix:部署2

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 const app = new Koa();
+const router = new Router();
 
-app.use(async ctx => {
-  ctx.body = 'Hello Vercel';
+//
+router.get('/api', async (ctx, next) => {
+  ctx.body = {
+    name: '张三'
+  }
 });
 
+// 注册中间件
+app.use(bodyParser());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+// app.use(async ctx => {
+//   ctx.body = 'Hello Vercel';
+// });
+
+
 app.listen(3008, () => {
-  console.log('3008项目启动')
+  console.log('3008项目启动', 'http://localhost:3008')
 });
