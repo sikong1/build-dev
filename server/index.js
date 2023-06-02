2f3abf26f822897c3806015e84bbe114dd0cd538
sikonggpw <1327325804@qq.com>
2023-06-02 15:18:36 +0800

    fix:部署14

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Home Page Route'));
+// app.get('/', (req, res) => res.send('Home Page Route'));
 
 app.get('/about', (req, res) => res.send('About Page Route'));
 
