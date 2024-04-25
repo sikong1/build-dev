c64138e15fcf261bd8e9c2eb497ff1f020663bfd
sikonggpw <1327325804@qq.com>
2024-04-25 11:52:34 +0800

    fix: 增加ws

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
  * @Author: sikonggpw 1327325804@qq.com
  * @Date: 2022-10-27 23:39:51
  * @LastEditors: sikonggpw 1327325804@qq.com
- * @LastEditTime: 2023-09-11 17:30:15
+ * @LastEditTime: 2024-04-25 11:52:04
  * @FilePath: \snow-vue\vue.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -79,6 +79,7 @@ module.exports = defineConfig({
         target: baseUrl,
         // target: 'https://guopw-api.club',
         changeOrigin: true,
+        ws: true,
         pathRewrite: {
           "^/api": "/"
         }
